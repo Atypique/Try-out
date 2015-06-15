e00f6ad628f9aac4f1896a8af88c64a79158dc5f
Atypique <blanchard.jeremy29@gmail.com>
2015-06-15 12:13:19 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tryout::Application.config.secret_key_base = 'e67f883a07fa822cc7186c28328d35bc81e9711e6fe04262bdeabbf662b1a5611bda4aeaa4a75d24e3c13c4c953feb97e7b2ac502e0cfb97e96c12fd3bdce670'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tryout::Application.config.session_store :cookie_store, key: '_Tryout_session'
