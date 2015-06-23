08a6d3d186b949291bff4dc02df06055f5fc92cc
Atypique <blanchard.jeremy29@gmail.com>
2015-06-23 19:26:36 +0200

    general update of ruby and rails / resetup of dropbox keys

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ActiveRecord::Base
   has_attached_file :images, :storage => :dropbox,
     				:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    				:styles => { :medium => "700x>", :thumb => "100x100>" }, :default_url => "No_image_available.png"
+    				:styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "No_image_available.png"
   validates_attachment_content_type :images, :content_type => /\Aimage\/.*\Z/
 end
