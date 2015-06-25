7f8f93a3c3bb95f3d493653528e21e710d7fddf0
Atypique <blanchard.jeremy29@gmail.com>
2015-06-25 17:35:39 +0200

    Separating dropbox from local host

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,12 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :images, :storage => :dropbox,
-    				:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    				:styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "No_image_available.png"
-  validates_attachment_content_type :images, :content_type => /\Aimage\/.*\Z/
+	if Rails.env.developent?
+	  has_attached_file :images,
+	  validates_attachment_content_type :images, :content_type => /\Aimage\/.*\Z/
+	else 
+	  has_attached_file :images, :storage => :dropbox,
+	    				:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+	    				:styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "No_image_available.png",
+	    				:path => ":style/:id_:filename" #This creates a folder with the style name first (medium / thumb) and then writes the file name with a unique ID
+	  validates_attachment_content_type :images, :content_type => /\Aimage\/.*\Z/
+	end
 end
